4ae2ce1678def0e09eb727f4e7e728c555e82184
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-11-08 22:33:46 +0000

    allpages

---
+++ client/src/pages/StudyLog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudyLog: React.FC = () => {
+  return (
+    <div style={{ padding: 32, fontFamily: 'Inter, system-ui, sans-serif' }}>
+      <h1>PageSample</h1>
+    </div>
+  )
+}
+
+export default StudyLog
