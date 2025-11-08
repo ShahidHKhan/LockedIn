d31bff1b19fc2252f324e04aeca33dfa6813c09c
Christian Barberan <144821156+WestwardSky8392@users.noreply.github.com>
2025-11-08 22:37:33 +0000

    Created Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,8 +7,11 @@ import { useNavigate, Link } from 'react-router-dom'
 const styles: Record<string, React.CSSProperties> = {
   container: { maxWidth: 880, margin: '48px auto', padding: 24, borderRadius: 8, fontFamily: 'Inter, system-ui, sans-serif' },
   header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 20 },
-  card: { padding: 18, borderRadius: 8, boxShadow: '0 4px 18px rgba(0,0,0,0.04)' },
-  btn: { padding: '8px 12px', borderRadius: 6, border: 'none', background: '#ef4444', color: '#fff', cursor: 'pointer' }
+  card: { paddingBottom:'130px',padding: 18, borderRadius: 8, boxShadow: '0 4px 18px rgba(0,0,0,0.04)' },
+  btn: { padding: '8px 12px', borderRadius: 6, border: 'none', background: '#ef4444', color: '#fff', cursor: 'pointer' },
+  btn2:{ fontSize: '30px',padding: '8px 100px', borderRadius: 6, border: 'none', background: '#ff7231ff', color: '#fff', cursor: 'pointer'},
+  btn3:{ fontSize: '30px',padding: '8px 114px', borderRadius: 6, border: 'none', background: '#ff7231ff', color: '#fff', cursor: 'pointer'},
+  btnPos:{ paddingTop:'250px',paddingBottom:'30px'},
 }
 
 const Home: React.FC = () => {
@@ -51,7 +54,8 @@ const Home: React.FC = () => {
 
       <div style={styles.card}>
         <h3>Welcome{user ? `, ${user.displayName || ''}` : ''} 👋</h3>
-        <p>This is the home page — protected by Firebase Authentication.</p>
+        <div style={styles.btnPos}><button style={styles.btn2}>Study</button></div>
+        <div><button style={styles.btn3}>Log</button></div>
       </div>
     </div>
   )
